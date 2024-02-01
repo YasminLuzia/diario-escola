9432fbde41f859fd5c909ebe9294e0f9c9507037
Yasmin S Luzia <yasmin.s.luzia@gmail.com>
2024-01-31 21:12:06 -0300

    Adicionado select de todos dados

--- parentesco.sql
+++ parentesco.sql
@@ -42,3 +42,22 @@ FROM
     LEFT JOIN responsavel res2 ON par2.idResponsavel = res2.id
 WHERE
     par1.idResponsavel <> par2.idResponsavel group by aluno;
+
+
+SELECT
+    alu.id AS IdAluno,
+    alu.nome AS Aluno,
+    res1.id as IdResponsavel1,
+    res1.nome AS NomeResponsavel1,
+    par1.parentesco AS Parentesco1,
+    res2.id as IdResponsavel2,
+    res2.nome AS NomeResponsavel2,
+    par2.parentesco AS Parentesco2
+FROM
+    aluno alu
+    LEFT JOIN parentesco par1 ON alu.id = par1.idAluno
+    LEFT JOIN responsavel res1 ON par1.idResponsavel = res1.id
+    LEFT JOIN parentesco par2 ON alu.id = par2.idAluno
+    LEFT JOIN responsavel res2 ON par2.idResponsavel = res2.id
+WHERE
+    par1.idResponsavel <> par2.idResponsavel group by aluno;
